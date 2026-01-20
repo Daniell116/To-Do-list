3f20eff90293d29189e48ca2351cdb1958cc9c9a
daniell escareno <daniellescareno7@gmail.com>
2026-01-20 08:28:39 -0800

    Initial Commit

---
+++ To-Do list/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do list
+//
+//  Created by daniell Escareno on 1/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do list/To_Do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_listApp.swift
+//  To-Do list
+//
+//  Created by daniell Escareno on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
