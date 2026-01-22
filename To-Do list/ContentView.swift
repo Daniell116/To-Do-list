643d41361e1b901ca1b7521877332e09bfc5bcbc
daniell escareno <daniellescareno7@gmail.com>
2026-01-22 06:39:51 -0800

    Set up edit list edit functionality

--- To-Do list/ContentView.swift
+++ To-Do list/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var things = ["First thing", "Second thing" ,"Third thing" ,"Fourth thing", "Fifth thing"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(things, id: \.self) { item in
+                    Text(item.description)
+                }
+                .onMove(perform: { indices, newOffset in
+                    things.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { IndexSet in
+                    things.remove(atOffsets: IndexSet)
+                })
+            }
+            .navigationBarTitle("Things", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
