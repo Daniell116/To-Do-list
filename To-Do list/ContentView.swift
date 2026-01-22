0212d03adfd73919bf8d6e1955248119f665e608
daniell escareno <daniellescareno7@gmail.com>
2026-01-22 12:21:53 -0800

    Redesigned list display

--- To-Do list/ContentView.swift
+++ To-Do list/ContentView.swift
@@ -9,24 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var toDoItems =
-            [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
-             ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
-             ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+    [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+     ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+     ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
         NavigationView {
             List {
                 ForEach(toDoItems) { item in
-                    Text(item.description)
+                    HStack{
+                        VStack(alignment: .leading, content: {
+                            Text(item.priority).font(.headline)
+                            Text(item.description)
+                        })
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
                 }
-                .onMove(perform: { indices, newOffset in
-                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
-                })
-                .onDelete(perform: { IndexSet in
-                    toDoItems.remove(atOffsets: IndexSet)
-                })
-            }
-            .navigationBarTitle("To do list", displayMode: .inline)
-            .navigationBarItems(leading: EditButton())
+                    .onMove(perform: { indices, newOffset in
+                        toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                    })
+                    .onDelete(perform: { IndexSet in
+                        toDoItems.remove(atOffsets: IndexSet)
+                    })
+                }
+                .navigationBarTitle("To do list", displayMode: .inline)
+                .navigationBarItems(leading: EditButton())
+            
         }
     }
 }
